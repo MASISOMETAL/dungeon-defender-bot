1ab7a5fb55679d757fccf90175e15c017f3dc60c
MASISOMETAL <cristianmasisofarias@gmail.com>
2025-06-01 19:05:18 -0300

    agregue bonus command

---
+++ commands/bonus.js
@@ -0,0 +1,20 @@
+export default {
+  name: 'bonus',
+  async execute(message) {
+    const args = message.content.slice(8).trim().split(' ').map(Number);
+
+    // ✅ Validación: asegurarse de que hay exactamente 4 números y son válidos
+    if (args.length !== 4 || args.some(isNaN)) {
+      return message.reply('⚠ Debes ingresar exactamente **4 números**. Ejemplo: `<<bonus 100 100 100 100`');
+    }
+
+    // 🔢 Calcular la suma total
+    const sumaTotal = args.reduce((acc, num) => acc + num, 0);
+    const bonus = (sumaTotal * 40) / 100
+
+    // 🟢 Enviar el resultado
+    message.reply(`🎲 **Estadisticas de la armadura:** \`${sumaTotal}\`\n` +
+      `🎲 **Bonus total:** \`${Math.round(sumaTotal + bonus)}\``
+    );
+  }
+};
